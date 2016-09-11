31186552cc25d8946c78937114128308de0c782c
Jason Sutter <jasonsutter87@gmail.com>
2016-09-10 21:27:08 -0700

    Updated home page, created users log in and sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
    get '/' => 'sessions#index'
+   get '/sessions/new' => 'sessions#new'
+   get '/users/new' => 'users#new'
 end
