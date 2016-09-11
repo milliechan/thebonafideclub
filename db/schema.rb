45a05f213aba1a739276487b21ffda6d6df14cc6
Jason Sutter <jasonsutter87@gmail.com>
2016-09-10 23:43:56 -0700

    Updated migration and nav bar

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20160911001026) do
-
-
+ActiveRecord::Schema.define(version: 20160911004031) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,36 +30,18 @@ ActiveRecord::Schema.define(version: 20160911001026) do
     t.datetime "updated_at", null: false
   end
 
-
-  create_table "models", force: :cascade do |t|
-    t.string   "name",                   default: "",                                                                                                   null: false
-    t.integer  "age",                                                                                                                                   null: false
-    t.string   "location",               default: "",                                                                                                   null: false
-    t.text     "bio",                    default: "",                                                                                                   null: false
-    t.string   "user_img",               default: "http://www.ohioattorneygeneral.gov/getattachment/732f8bef-858c-4581-9e5c-768687c6df35/picture.aspx", null: false
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "password_digest"
+    t.string   "name"
+    t.integer  "age"
+    t.string   "picture"
+    t.string   "location"
+    t.text     "bio"
     t.string   "latitude"
     t.string   "longitude"
-    t.string   "email",                  default: "",                                                                                                   null: false
-    t.string   "encrypted_password",     default: "",                                                                                                   null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,                                                                                                    null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.inet     "current_sign_in_ip"
-    t.inet     "last_sign_in_ip"
-    t.string   "Users"
-    t.datetime "created_at",                                                                                                                            null: false
-    t.datetime "updated_at",                                                                                                                            null: false
-    t.index ["email"], name: "index_models_on_email", unique: true, using: :btree
-    t.index ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true, using: :btree
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
-
-  create_table "users", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
 
 end
